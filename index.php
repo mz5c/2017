1cbe81626408d2358aab27b951669f8463bc2a23
iamrico <w11w23c58@126.com>
2017-01-25 11:03:18 +0800

    test

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
+session_start();
+$_SESSION['abc'] = empty($_GET['abc']) ? mt_rand(100000, 999999) : $_GET['abc'];
 echo 'hello world !!!' . "\n";
-echo 'now is ' . date('Y-m-d H:i:s');
+echo 'now is ' . date('Y-m-d H:i:s') . "\n";
+echo 'abc = ' . $_SESSION['abc'];
